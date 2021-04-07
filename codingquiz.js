a070c20c8659f973efb85c04611c070eb7ef0de6
SolomonL6 <81569452+SolomonL6@users.noreply.github.com>
2021-04-07 00:04:18 -0500

    Add files via upload

---
+++ codingquiz.js
@@ -0,0 +1,8 @@
+//create start button and add click event listener to it.
+
+addEventListener("click", startQuiz)
+ //after clicked start the timer with 30 seconds.
+  //if timer === 0 
+    // Show input input box for user name initials
+     //submit button takes to next html page. With the user initials and score.
+       // next html page has list of all users with the score. + go back to original page button + clear score button
